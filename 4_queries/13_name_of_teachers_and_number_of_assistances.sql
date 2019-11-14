3928242a75fe422054dcf4cdd4d72e222eb9d325
stella-zb <upperdaystudio@gmail.com>
2019-11-14 04:30:26 +0000

    correct typo error

--- 4_queries/13_name_of_teachers_and_number_of_assistances.sql
+++ 4_queries/13_name_of_teachers_and_number_of_assistances.sql
@@ -1,6 +1,6 @@
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort, count(assistance_requests) as total_assistances
 FROM teachers
-JOIN assistant_requests ON teacher_id = teachers.id
+JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
 WHERE cohorts.name = 'JUL02'
